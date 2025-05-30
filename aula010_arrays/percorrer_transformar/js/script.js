9005f63b4dc648dd11a7a9e1ea9d1f9abd80db9a
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-05-29 21:39:34 -0300

    #012 - Aula010: Arrays - Iteração e Transformação - Início

---
+++ aula010_arrays/percorrer_transformar/js/script.js
@@ -0,0 +1,14 @@
+// Dados de Exemplo
+
+// Array de números para exemplos básicos
+const numeros = [1, 2, 3, 4, 5];
+
+// Array de objetos (pessoas) para exemplos mais complexos
+const pessoas = [
+    {nome: 'Gal Gadot', idade: 40, cidade: 'Rosh HaAyin'},
+    {nome: 'Elizabeth Olsen', idede: 36, cidade: 'Sherman Oaks'},
+    {nome: 'Alexandra Daddario', idede: 39, cidade: 'Nova York'},
+    {nome: 'Anna de Armas', idede: 37, cidade: 'Havana'},
+    {nome: 'Grace Kelly', idede: 95, cidade: 'Filadélfia'},
+    {nome: 'Jaz Sinclair', idede: 17, cidade: 'Dallas'},
+];
